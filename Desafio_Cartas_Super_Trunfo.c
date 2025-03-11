ef3188d231d75be74698231eff9d38ea2dc07a35
KaikeTL <kaikeleal@gmail.com>
2025-03-11 10:57:13 +0000

    falta fazer o printf das 2 cartas

--- Desafio_Cartas_Super_Trunfo.c
+++ Desafio_Cartas_Super_Trunfo.c
@@ -1,14 +1,67 @@
 #include <stdio.h>
 int main(){
 
+// Váriavei
 
-    char Estado, Codigo, Nome_da_cidade [50];
-    int Populacao, Pontos_turisticos; 
-    float Area_em_Km, Pib;
+
+    char Estado, Codigo, Nome_da_Cidade [100];
+    int Populacao, Pontos_Turisticos; 
+    float Area, Pib;
 
     printf("Carta 1 \n");
-    printf("Digite o Estado: dsa");
+    printf("Digite o Estado: ");
     scanf("%s", &Estado);
+
+    printf("Digite o Código: ");
+    scanf("%s",&Codigo);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf("%s",&Nome_da_Cidade);
+
+    printf("Digite a População: ");
+    scanf("%d", &Populacao);
+
+    printf("Digite a Área: ");
+    scanf("%f", &Area);
+
+    printf("Digite o Pib: ");
+    scanf("%f", &Pib);
+
+    printf("Digite o Numero de Pontos Turísticos: " );
+    scanf("%d", &Pontos_Turisticos);
+
+
+
+
+    printf("Carta 2 \n");
+    printf("Digite o Estado: ");
+    scanf("%s", &Estado);
+
+    printf("Digite o Código: ");
+    scanf("%s",&Codigo);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf("%s",&Nome_da_Cidade);
+
+    printf("Digite a População: ");
+    scanf("%d", &Populacao);
+
+    printf("Digite a Área: ");
+    scanf("%f", &Area);
+
+    printf("Digite o Pib: ");
+    scanf("%f", &Pib) ;
+
+    printf("Digite o Numero de Pontos Turísticos: ");
+    scanf("%d", &Pontos_Turisticos \n);
+
+
+    printf("Carta 1 \n");
+    
+
+
+
+
 
 
 
