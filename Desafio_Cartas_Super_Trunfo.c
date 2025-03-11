abae24c7911c196b447190735d1af61a685a87e1
KaikeTL <kaikeleal@gmail.com>
2025-03-11 10:14:32 +0000

    adicionei a primeira string

---
+++ Desafio_Cartas_Super_Trunfo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+
+
+    char Estado, Codigo, Nome_da_cidade [50];
+    int Populacao, Pontos_turisticos; 
+    float Area_em_Km, Pib;
+
+    printf("Carta 1 \n");
+    printf("Digite o Estado: dsa");
+    scanf("%s", &Estado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
